c50f56831abd93fd491053f799369dff2aa14af2
wutong_i <wutong_i@didichuxing.com>
2017-02-13 10:05:42 +0800

    part6-计算属性

---
+++ demoDB.js
@@ -0,0 +1,26 @@
+var games = [
+  {
+    'id': 01,
+    'name': 'itme01',
+    'price': 120.00,
+    'purchased': false
+  },
+  {
+    'id': 02,
+    'name': 'itme02',
+    'price': 1250.00,
+    'purchased': false
+  },
+  {
+    'id': 03,
+    'name': 'itme03',
+    'price': 1120.00,
+    'purchased': false
+  },
+  {
+    'id': 04,
+    'name': 'itme04',
+    'price': 20.50,
+    'purchased': false
+  },
+]
--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 var data = {
-  info: 'data-info,obj'
+  games: window.games,
+  cash: 5000,
 }
 var app = new Vue({
   el: '#app',
-  data: data
+  data: data,
+  computed: {
+    wishList: function () {
+      return this.games.filter(function (game) {
+        return !game.purchased
+      })
+    },
+    myGames: function () {
+      return this.games.filter(function (game) {
+        return game.purchased
+      })
+    },
+    balance: function () {
+      let sum = 0
+      this.myGames.forEach(function (item, index) { 
+        sum += item.price
+      })
+      return this.cash - sum
+    }
+  },
+  methods: {
+    buyItems: function (id) {
+      this.games[--id].purchased = true
+    },
+    rmItems: function (id) {
+      this.games[--id].purchased = false
+    }
+  }
 });
